783a10d7f1a7e01d0ed5de591436a917c3f6b8bf
NeilShankar <neilshankarnath@gmail.com>
2020-06-07 12:46:49 +0530

    Testing

--- next.config.js
+++ next.config.js
@@ -6,6 +6,7 @@ const webpack = require('webpack');
 const apiKey =  JSON.stringify(process.env.SHOPIFY_API_KEY);
 
 module.exports = withCSS(withSass({
+  useFileSystemPublicRoutes: false,
   webpack: (config) => {
     const env = { API_KEY: apiKey };
     config.plugins.push(new webpack.DefinePlugin(env));
